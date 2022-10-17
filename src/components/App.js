a2913e50dd2026eecea3330cc9fb2b349114ca8f
Veronica isla <veroislagleaniz@gmail.com>
2022-10-17 13:49:01 +0200

    started validation form

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,68 @@
 import React from 'react';
+import { useState } from 'react';
 import Info from '../components/Info';
 import PurpleContainer from '../components/PurpleContainer';
 import Form from '../components/Form';
 import '../styles/components/App.scss';
 
 function App() {
+  const [dataUser, setDataUser] = useState({
+    name: '',
+    lastName: '',
+    email: '',
+    password: '',
+  });
+
+  //VALIDACIÓN FORMULARIO
+  const [errors, setErrors] = useState({
+    name: '',
+    lastName: '',
+    email: '',
+    password: '',
+  });
+
+  //FUNCIÓN PREVENIR ENVÍO POR DEFECTO
+  const handleSubmit = (ev) => {
+    ev.preventDefault(ev);
+  };
+
+  // GUARDAR VALOR INPUT
+
+  const handleInput = (ev) => {
+    setDataUser(ev.target.value);
+  };
+
+  //FUNCION QUE LLAMA A VALOR INPUT Y A VALIDACION
+
+  const handleForm = (ev) => {
+    handleInput(ev);
+    setErrors(validateForm(dataUser));
+  };
+
+  //VALIDACIONES
+
+  const validateForm = (dataUser) => {
+    let errors = {};
+    let regexName = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+
+    if (dataUser.name === '') {
+      errors.name = 'Can´t be blank';
+    } else if (!regexName.test(dataUser.name)) return errors;
+  };
+
   return (
     <>
       <div className="body">
         <Info />
         <div className="container">
           <PurpleContainer />
-          <Form />
+          <Form
+            handleInput={handleInput}
+            handleSubmit={handleSubmit}
+            dataUser={dataUser}
+            errors={errors}
+            handleForm={handleForm}
+          />
         </div>
       </div>
     </>
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import '../styles/components/Form.scss';
 
-function Form() {
+function Form(props) {
+  const handleInput = (ev) => {
+    const inputValue = ev.target.value;
+    const inputChange = ev.target.name;
+    props.handleInput(inputValue, inputChange);
+  };
   return (
     <section className="form">
       <form
@@ -9,36 +14,46 @@ function Form() {
         action=""
         method="POST"
         name="signupForm"
+        onSubmit={props.handleSubmit}
       >
         <input
           className="form__input"
           type="text"
-          name=""
-          id=""
+          name="name"
+          id="name"
           placeholder="First Name"
+          onChange={handleInput}
+          value={props.dataUser.name}
         />
+        {props.errors.name && (
+          <p className="form__error">{props.errors.name}</p>
+        )}
         <input
           className="form__input"
           type="text"
-          name=""
-          id=""
+          name="lastName"
+          id="lastName"
           placeholder="Last Name"
         />
         <input
           className="form__input"
           type="email"
-          name=""
-          id=""
+          name="email"
+          id="email"
           placeholder="Email Address"
         />
         <input
           className="form__input"
           type="password"
-          name=""
-          id=""
+          name="password"
+          id="password"
           placeholder="Password"
         />
-        <button className="form__submitBtn" type="submit">
+        <button
+          className="form__submitBtn"
+          type="submit"
+          onClick={props.handleForm}
+        >
           claim your free trial
         </button>
         <p className="form__termsContainer">
